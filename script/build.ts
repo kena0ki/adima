b4d5a478ce81c9d6a507989ef6613c64c400a7c7
kena0ki <ken3574.25@gmail.com>
2021-02-05 02:20:21 +0900

    Add esbuild

---
+++ script/build.ts
@@ -0,0 +1,16 @@
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+
+const argv = yargs(hideBin(process.argv)).argv;
+
+require('esbuild').build({
+  entryPoints: [argv.entryPoints || 'src/index.ts'],
+  bundle: true,
+  minify: true,
+  sourcemap: true,
+  target: ['es2019'],
+  outdir: argv.outdir || 'dist',
+  watch: 'watch' in argv,
+  format: 'esm',
+}).catch(() => process.exit(1));
+
